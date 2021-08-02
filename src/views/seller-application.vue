3116e6610057e7c3e416ec5d03868520a441c66a
Angel H <angel@adboomgrp.com>
2021-08-01 21:10:38 -0600

    added loader to both next and submit buttons on api requests

--- src/views/seller-application.vue
+++ src/views/seller-application.vue
@@ -177,15 +177,7 @@
             @click="nextClickHandler"
           >
             <span v-if="!showLoader">Next</span>
-            <svg
-              v-else
-              class="animate-spin mx-auto h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" 
-              fill="none" 
-              viewBox="0 0 24 24"
-            >
-              <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-              <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-            </svg>
+            <Loader v-else />
           </t-button>
         </div>
       </fieldset>
@@ -283,7 +275,8 @@
             class="mx-auto w-full sm:w-auto font-semibold bg-mock-green sm:float-right pl-9 pr-9"
             @click="submitClickHandler"
           >
-            Submit Application
+            <span v-if="!showLoader">Submit Application</span>
+            <Loader v-else />
           </t-button>
           <router-link 
             class="flex justify-center md:block sm:float-left mt-3" 
@@ -305,11 +298,13 @@
 <script>
 import { httpClient } from '@/services'
 import Steps from '@/components/steps.vue'
+import Loader from '@/components/loader.vue'
 
 export default {
   name: 'SellerApplication',
   components: {
-    Steps
+    Steps,
+    Loader
   },
   data () {
     return {
@@ -340,6 +335,7 @@ export default {
     portfolioUrlText () {
       if (this.duplicatePortfolio) {
         this.duplicatePortfolio = false
+        this.applicationData.isAuthor = null
       }
     },
   },
